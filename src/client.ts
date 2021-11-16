e7297543eae976e34d0ad9780e2345a6d67fa6c9
oceanroleplay <indianoceanroleplay@gmail.com>
2021-11-16 13:46:46 +0530

    refactor: comment botguilds

--- src/client.ts
+++ src/client.ts
@@ -9,11 +9,13 @@ const client = new Client({
   },
   intents: [
     Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
     Intents.FLAGS.GUILD_VOICE_STATES,
   ],
-  botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+  // If you only want to use guild commands, uncomment this line
+  // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
   silent: true,
 });
 
